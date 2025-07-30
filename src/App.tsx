a5f3218d12d6124ec3df49212c75c940c59ea143
Johao Lopez <johao.lopz.m@gmail.com>
2025-07-29 22:39:19 -0500

    animation with gsap

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import gsap from 'gsap';
 
 function App() {
+  const initNumber = 1;
+  const finalNumber = 3;
+  const [ secJump, setSecJump ] = useState<number>(initNumber);
+  const jumperElementRef = useRef<HTMLHeadingElement>(null);
+  const intervalSecs: number = 1000;
+  const timelineGsap = gsap.timeline();
+
+  secJump === finalNumber && (() => {
+    timelineGsap.to('h2', { y: -10 })
+      .to('h2', { y: 0, ease: 'bounce.out' });
+  })()
+
+  useEffect(() => {
+    if (secJump >= finalNumber) return; // Detener si alcanzó el límite
+
+    const intervalId = setInterval(() => {
+      setSecJump(prev => {
+        if (prev < finalNumber) {
+          return prev + 1;
+        } else {
+          clearInterval(intervalId); // Por si acaso
+          return prev;
+        }
+      });
+    }, intervalSecs);
+
+    return () => clearInterval(intervalId); // Limpieza del intervalo
+  }, [secJump, finalNumber]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +49,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <main>
+        <h1>Prueba GSAP</h1>
+        <h2 ref={ jumperElementRef }>{ secJump }</h2>
+        <button
+          onClick={() => {
+            setSecJump(initNumber);
+          }}
+        >
+          Refresh
+        </button>
+      </main>
     </div>
   );
 }
